e772fb40543c0cb6b6f29bdf5ab970d923d561c4
Polina Skuratova <skuratova250496@gmail.com>
2022-12-03 12:59:03 +0700

    Initial Commit

---
+++ Pokemons/PokemonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonsApp.swift
+//  Pokemons
+//
+//  Created by Поляндий on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
