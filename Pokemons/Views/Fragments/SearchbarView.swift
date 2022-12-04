0d9d02a5d5e347f57a3f8b302d9594f5baf50897
Polina Skuratova <skuratova250496@gmail.com>
2022-12-04 14:35:33 +0700

    custom SearchbarView

---
+++ Pokemons/Views/Fragments/SearchbarView.swift
@@ -0,0 +1,57 @@
+//
+//  SearchbarView.swift
+//  Pokemons
+//
+//  Created by Поляндий on 04.12.2022.
+//
+
+import SwiftUI
+
+struct SearchbarView: View {
+    @Binding var boxNumber: Int
+    @Binding var searchedPokemon: String
+    @Binding var showView: Bool
+    @ObservedObject var viewModel: HomeViewModel
+
+    var body: some View {
+        VStack(alignment: .trailing) {
+            Text("Back")
+                .font(.system(size: 20))
+                .padding(.init(top: 4, leading: 8, bottom: 4, trailing: 8))
+                .background(Color.white)
+                .cornerRadius(12)
+                .padding(.trailing, 20)
+                .onTapGesture {
+                    showView = false
+                }
+            
+            HStack {
+                TextField("Pokemon name", text: $searchedPokemon, onCommit: {
+                    searchPokemon()
+                })
+                .padding(.init(top: 0, leading: 18, bottom: 0, trailing: 5))
+                
+                Image("ic_search").resizable().frame(width: 35, height: 35).foregroundColor(.white)
+                    .onTapGesture {
+                        searchPokemon()
+                    }
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .withRoundedCornersAndPadding(backgroundColor: Color("endBackgroundGradient"))
+            .padding(.init(top: 0, leading: 20, bottom: 0, trailing: 20))
+        }
+    }
+    
+    private func searchPokemon(){
+        if(searchedPokemon.isEmpty){
+            resetPokemonList()
+        }else{
+            viewModel.getPokemonDetailByName(name: searchedPokemon.lowercased())
+        }
+    }
+    
+    private func resetPokemonList(){
+        boxNumber = 1
+        viewModel.getPokemonsFromAPI(from: 0)
+    }
+}
